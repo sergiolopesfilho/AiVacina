ae9bac62274aa6a8af481aa1b86b1ce39990e39d
m4rco8ilva <m4rco8ilva@users.noreply.github.com>
2017-09-25 23:01:43 -0300

    Teste - Adicionando Vacina

---
+++ AiVacina/AiVacina/Models/Vacina.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AiVacina.Models
+{
+    public class Vacina
+    {
+        public int codVacina { get; set; }
+
+        public int loteVacina { get; set; }
+
+        public string nomeVacina { get; set; }
+
+        public int quantidade { get; set; }
+
+        public DateTime dataValidade { get; set; }
+
+        public string grupoAlvo { get; set; }
+
+    }
+}
